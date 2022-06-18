159ca57f8baa11668032231da22e9e8eaac86e23
mpdla <m.poncedeleon.aguilar@hotmail.com>
2022-06-18 14:57:58 -0500

    Probando a poner 3 piezas juntas con join

--- figura4.c
+++ figura4.c
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 
 void display(){
+  //Fila de tablero
   char** blackSquare = reverse(whiteSquare);
   char** twoSquares = join(blackSquare, whiteSquare);
   free(blackSquare);
   char** row = repeatH(twoSquares, 4);
   free(twoSquares);
-  interpreter(row);
+  //Piezas
+  char** pieces = join(join(rook, knight), bishop);
+  
+  interpreter(pieces);
   free(row);
+  free(pieces);
 }
