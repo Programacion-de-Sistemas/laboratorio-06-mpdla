280dee9ad7807ae70095ac58eadd400cc3fd7b35
mpdla <m.poncedeleon.aguilar@hotmail.com>
2022-06-18 15:04:30 -0500

    Realizando la figura 5, que es tal cual como la 4, solo se invierten los colores de las piezas con reverse

---
+++ figura5.c
@@ -0,0 +1,21 @@
+#include "chess.h"
+#include "figures.h"
+#include <stdlib.h>
+
+void display(){
+  //Fila de tablero
+  char** blackSquare = reverse(whiteSquare);
+  char** twoSquares = join(blackSquare, whiteSquare);
+  free(blackSquare);
+  char** row = repeatH(twoSquares, 4);
+  free(twoSquares);
+  //Piezas
+  char** pieces = join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook);
+  pieces = reverse(pieces);
+  //Figura completa
+  char** figure = superImpose(pieces, row);
+  free(row);
+  free(pieces);
+  interpreter(figure);
+  free(figure);
+}
