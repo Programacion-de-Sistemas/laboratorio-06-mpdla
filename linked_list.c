afe68c2c1f7d9883498cf016d405fdf5cf469e6c
mpdla <m.poncedeleon.aguilar@hotmail.com>
2022-06-19 19:09:01 -0500

    Creando función para agregar un elemento a la lista

--- linked_list.c
+++ linked_list.c
@@ -1,54 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//definimos nuestra node
+//Definición de node
 typedef struct node
 {
-    int number;
-    struct node *next;
+  int number;
+  struct node *next;
 }
 node;
 
+node *addElement(node *list, int element){
+  node *n = malloc(sizeof(node)); //Asignando memoria para node
+  if(n==NULL) printf("No se pudo agregar el elemento\n");
+  else{
+    n->number = element;
+    n->next = NULL;
+    list = n;
+  }
+  return list;
+}
+
 int main(){
-    node *list=NULL;
+  node *list=NULL;
 
-    //agregamos un elemento a la lista
-    node *n = malloc(sizeof(node)); //asignando memoria para node
-    if(n==NULL){
-        return 1;
-    }
-    n->number=45;
-    n->next=NULL;
+  list = addElement(list, 45);
+  list->next = addElement(list, 15);
 
-    node *n_2 = malloc(sizeof(node)); //asignando memoria para node
-    if(n_2==NULL){
-        return 1;
-    }
-    n_2->number=15;
-    n_2->next=NULL;
+  //Imprimiendo los elementos de la lista
+  for(node *tmp = list; tmp != NULL; tmp = tmp->next){
+    printf("%i\n",tmp->number);
+  }
 
-    //actualizar el punto next del primer elemento
-    n->next=n_2;
+  //Liberando memoria
+  while(list!=NULL){
+    node *tmp = list->next;
+    free(list);
+    list=tmp;
+  }
 
-
-    //actualizamos la lista
-    list=n;
-
-    // imprimir los elementos de la lista
-    for (node *tmp = list;tmp!=NULL;tmp=tmp->next)
-    {
-        printf("%i\n",tmp->number);
-
-
-    }
-
-    // función (free) BUENA PR´ACTICA EN PROGRAMACI´ON EN "C"
-    while(list!=NULL){
-        node *tmp=list->next;
-        free(list);
-        list=tmp;
-    }
-
-    return 0;
-
+  return 0;
 }
