88c2355dd315dfa73b8f9bc2535e0a2c57af3954
mpdla <m.poncedeleon.aguilar@hotmail.com>
2022-06-19 20:18:50 -0500

    Agregando varios elementos ingresados por el usuario, con for

--- linked_list.c
+++ linked_list.c
@@ -29,12 +29,17 @@ node *addElement(node *list, int element){
 
 int main(){
   node *list = NULL;
-  int element;
-  printf("Ingrese un elemento: ");
-  scanf("%d", &element);
-  list = addElement(list, element);
-  list = addElement(list, element + 1);
-  list = addElement(list, element + 2);
+  int quantity, element;
+
+  //Agregando elementos a la lista
+  printf("Cantidad de elementos: ");
+  scanf("%d", &quantity);
+  for(unsigned int i = 0; i < quantity; i++){
+    printf("Ingrese elemento: ");
+    scanf("%d", &element);
+    list = addElement(list, element);
+  }
+
   //Imprimiendo los elementos de la lista
   printf("Lista:\n");
   for(node *tmp = list; tmp != NULL; tmp = tmp->next){
